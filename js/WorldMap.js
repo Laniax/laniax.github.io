7f10b6b4d3a13c2825f441bc28474e6f634488a4
Bas <bnoorlander@hotmail.com>
2015-05-08 03:08:49 +0200

    Initial commit

---
+++ js/WorldMap.js
@@ -0,0 +1,15 @@
+function WorldMap()
+{
+	
+	var TILE_SIZE = 3;
+    var TILE_X_MODIFIER = 1984;
+    var TILE_Y_MODIFIER = -1153;
+	var MAP_HEIGHT = 5376;
+	
+	this.coordsToPoint = function(xcoord, ycoord) 
+	{
+		return {
+		x: ((xcoord - TILE_X_MODIFIER) * TILE_SIZE), 
+		y: ((MAP_HEIGHT - (ycoord - TILE_Y_MODIFIER)) * TILE_SIZE)};
+	}
+};
